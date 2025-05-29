46c61b8298ebcf77e32ca007c7ca39a48fae7797
Maciej <keicamboom@gmail.com>
2025-05-29 12:43:35 +0200

    Modyfikacja CountryInfo.css [dodanie listy miast]

--- Assets/Scripts/CountryInfo.cs
+++ Assets/Scripts/CountryInfo.cs
@@ -1,12 +1,14 @@
 // Nazwa pliku: CountryInfo.cs (jeœli tworzysz osobny plik)
 
 using System; // Potrzebne dla atrybutu Serializable
+using System.Collections.Generic; //dla listy
 
 [Serializable] // To jest wa¿ne, aby JsonHelper móg³ j¹ deserializowaæ
 public class CountryInfo
 {
     public string nazwa;
     public string stolica;
+    public List<string> miasta;
     public string powitanie;
     public string symbol;
     public string symbolPlik;
